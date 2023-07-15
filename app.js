f1a400417a3c29b7c70c79f2eeb8e1d0b3070191
Anshu <meanshuraj001@gmail.com>
2023-07-16 00:55:21 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express=require("express");
+const path=require("path");
+const fs=require("fs");
+const app=express();
+const port=8000;
+
+//express specific stuff
+//app.use(express.static('static',options));
+app.use('/static',express.static('static'));
+app.use(express.urlencoded());
+
+//PUG specific stuff
+app.set('view engine','pug');
+app.set('views',path.join(__dirname,'views'));
+
+//ENDPOINT
+
+app.get('/',(req,res)=>{
+    const params={ };
+    res.status(200).render('index.pug',params);
+})
+
+
+
+
+//start the server
+app.listen(port,()=>{
+    console.log(`The application is running on the port ${port}`);
+})
